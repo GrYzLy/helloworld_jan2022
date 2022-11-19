a35f671eeb637cff18de5ba15c255090ae1f3a74
bartosz.rzemek@gmail.com <bartosz.rzemek@codecool.com>
2022-11-19 11:12:59 +0100

    Initial

---
+++ src/AppJan2022.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function AppJan2022() {
+  return (
+  <div>
+    <h1>Test2</h1>
+  </div>
+  );
+}
+
+export default AppJan2022;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import AppJan2022 from './AppJan2022';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppJan2022 />
   </React.StrictMode>
 );
 
